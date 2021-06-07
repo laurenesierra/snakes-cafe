d43f4a4d581fcc910115fc63979b7f16fd58c9e9
Lauren Sierra <laurenesierra@gmail.com>
2021-06-06 23:17:59 -0700

    started snakes cafe

---
+++ snakes-cafe/snakes_cafe/snakes_cafe.py
@@ -0,0 +1,114 @@
+stars38 = "*" * 38
+print(stars38)
+
+welcomemessage = "**" + "   " + "Weclome to the Snakes Cafe!" + "    " + "**"
+print(welcomemessage)
+
+seemenu = "**" + "   " + "Please see our menu below." + "     " + "**"
+print(seemenu)
+
+twostars = "**"
+print(twostars)
+
+quit = "**" + " " + 'To quit at any time, type "quit"' + " " + "**" 
+print(quit)
+
+stars38 = "*" * 38
+print(stars38)
+
+appetizers = "Appetizers"
+print(appetizers)
+
+lineborder = "----------"
+print(lineborder)
+
+wings = "Wings"
+print(wings)
+
+cookies = "Cookies"
+print(cookies)
+
+springrolls = "Spring Rolls"
+print(springrolls)
+
+print()
+
+entrees = "Entrees"
+print(entrees)
+
+lineborder = "-------"
+print(lineborder)
+
+salmon = "Salmon"
+print(salmon)
+
+steak = "Steak"
+print(steak)
+
+meattornado = "Meat Tornado"
+print(meattornado)
+
+literalgarden = "A Literal Garden"
+print(literalgarden)
+
+print()
+
+desserts = "Desserts"
+print(desserts)
+
+lineborder = "--------"
+print(lineborder)
+
+icecream = "Ice Cream"
+print(icecream)
+
+cake = "Cake"
+print(cake)
+
+pie = "Pie"
+print(pie)
+
+print()
+
+drinks = "Drinks"
+print(drinks)
+
+lineborder = "------"
+print(lineborder)
+
+coffee = "Coffee"
+print(coffee)
+
+tea = "Tea"
+print(tea)
+
+unicorntears = "Unicorn Tears"
+print(unicorntears)
+
+print()
+
+print()
+
+stars38 = "*" * 38
+print(stars38)
+
+orderprompt = "**" + "   " + "What would you like to order?" + "  " + "**"
+print(orderprompt)
+
+stars38 = "*" * 38
+print(stars38)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
